36eb0dea412712ec7c25417e01d4ae54b7adfca8
kbalsom <100210446+kbalsom@users.noreply.github.com>
2022-08-07 21:11:39 -0230

    Add files via upload

---
+++ components/logo.jsx
@@ -0,0 +1,11 @@
+import "../stylesheets/signuppage.css";
+
+function Logo() {
+  return (
+    <div className="logo">
+      <img src="https://unsplash.com/photos/gDPaDDy6_WE" alt="logo" />
+    </div>
+  );
+}
+
+export default Logo;
---
+++ components/navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../stylesheets/navbar.css";
+
+function NavBar() {
+  return (
+    <div className="navbar">
+      <h1>Home</h1>
+      <h1>Explore</h1>
+    </div>
+  );
+}
+
+export default NavBar;
