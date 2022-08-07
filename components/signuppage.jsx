93ded5cdf4343b04052b303fc275e652421bb320
kbalsom <100210446+kbalsom@users.noreply.github.com>
2022-08-07 21:10:19 -0230

    Update signuppage.jsx

--- components/signuppage.jsx
+++ components/signuppage.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import "../stylesheets/signuppage.css";
 
+import { Link } from "react-router-dom";
+
 function SignUpPage() {
   return (
     <div className="signuppage">
-      <h1>Sign Up Page</h1>
+      <h1>Sign Up</h1>
+      <input type="text" placeholder="E-mail" />
+      <input type="text" placeholder="Password" />
+
+      <Link to="/signup/createprofile">Sign Up</Link>
+      <h2> Already have an account?</h2>
+      <h1>Login</h1>
     </div>
   );
 }
