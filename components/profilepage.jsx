2cedd06462e6986c3786a913cbc2e8e07c6225a3
kbalsom <100210446+kbalsom@users.noreply.github.com>
2022-08-07 21:10:38 -0230

    Update profilepage.jsx

--- components/profilepage.jsx
+++ components/profilepage.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import "../stylesheets/profilepage.css";
+
+import { Link } from "react-router-dom";
+
 function ProfilePage() {
   return (
     <div className="profilepage">
-      <h1>Profile Page</h1>
+      <h1>Create Profile</h1>
+      <input type="text" placeholder="Username" />
+      <input type="text" placeholder="Preferred Pronouns" />
+      <input type="text" placeholder="Bio" />
+      <Link to="/signup/createprofile/feed">Confirm</Link>
     </div>
   );
 }
