b0f4d4220115cd4d0e27a0f9e7c29a4ff332f963
kbalsom <100210446+kbalsom@users.noreply.github.com>
2022-08-07 21:09:34 -0230

    Update App.js

--- App.js
+++ App.js
@@ -2,19 +2,19 @@ import "./App.css";
 import SignUpPage from "./components/signuppage";
 import ProfilePage from "./components/profilepage";
 import FeedPage from "./components/feedpage";
+import Logo from "./components/logo";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Logo />
         <Link to="/signup">Sign Up</Link> <br />
-        <Link to="/createprofile">Create Profile</Link> <br />
-        <Link to="/feed">Post Something</Link>
         <Routes>
           <Route path="/signup" element={<SignUpPage />} />
-          <Route path="/createprofile" element={<ProfilePage />} />
-          <Route path="/feed" element={<FeedPage />} />
+          <Route path="/signup/createprofile" element={<ProfilePage />} />
+          <Route path="/signup/createprofile/feed" element={<FeedPage />} />
         </Routes>
       </Router>
     </div>
