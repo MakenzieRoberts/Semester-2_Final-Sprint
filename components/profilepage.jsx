f3b6820432a4a9cae47ca2e9f5131374e2064970
kbalsom <100210446+kbalsom@users.noreply.github.com>
2022-08-09 12:08:44 -0230

    Update profilepage.jsx

--- components/profilepage.jsx
+++ components/profilepage.jsx
@@ -1,27 +1,33 @@
+import React from "react";
 import "../stylesheets/profilepage.css";
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function ProfilePage({ handleUserInfo, username, pronouns, bio }) {
-	function sendData(e) {
-		var un = document.querySelector("#username").value;
-		var pp = document.querySelector("#pronouns").value;
-		var bb = document.querySelector("#bio").value;
-		handleUserInfo(un, pp, bb);
-		e.preventDefault();
-	}
-	return (
-		<div className="profilepage">
-			<h1>Create Profile</h1>
-			<input type="text" id="username" placeholder="Username" />
-			<input type="text" id="pronouns" placeholder="Preferred Pronouns" />
-			<input type="text" id="bio" placeholder="Bio" />
-			{/* !KENZI : Fixed the button issue by nesting the button inside <Link> and changing 'onClick' to 'onSubmit' */}
-			<Link id="feed-link" to="/feed">
-				<button onSubmit={sendData}>Proceed</button>
-			</Link>
-		</div>
-	);
+  function sendData(e) {
+    var un = document.querySelector("#username").value;
+    var pp = document.querySelector("#pronouns").value;
+    var bb = document.querySelector("#bio").value;
+    handleUserInfo(un, pp, bb);
+    navigateToFeed();
+    e.preventDefault();
+  }
+  const navigate = useNavigate();
+
+  const navigateToFeed = () => {
+    navigate("/signup/createprofile/feed");
+  };
+
+  return (
+    <div className="profilepage">
+      <form>
+        <h1>Create Profile</h1>
+        <input type="text" id="username" placeholder="Username" />
+        <input type="text" id="pronouns" placeholder="Preferred Pronouns" />
+        <input type="text" id="bio" placeholder="Bio" />
+        <button onClick={sendData}>Submit</button>
+      </form>
+    </div>
+  );
 }
 
 export default ProfilePage;
