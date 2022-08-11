3d2bc56fa830f7cbb91a06bd4f35cb59e8d0e2bd
kbalsom <100210446+kbalsom@users.noreply.github.com>
2022-08-11 10:38:03 -0230

    Update App.js

--- App.js
+++ App.js
@@ -1,45 +1,50 @@
-import "./stylesheets/reset.css";
+import "./stylesheets/Reset.css";
 import "./App.css";
 
 import SignUpPage from "./components/SignUpPage";
 import ProfilePage from "./components/ProfilePage";
 import FeedPage from "./components/FeedPage";
 import Logo from "./components/Logo";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import LoginPage from "./components/LoginPage";
+import LoginFeedPage from "./components/LoginFeedPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useState } from "react";
 
 function App() {
-	const [username, setUsername] = useState("");
-	const [pronouns, setPronouns] = useState("");
-	const [bio, setBio] = useState("");
+  const [username, setUsername] = useState("");
+  const [pronouns, setPronouns] = useState("");
+  const [bio, setBio] = useState("");
 
-	function handleUserInfo(un, pp, bb) {
-		setUsername(un);
-		setPronouns(pp);
-		setBio(bb);
-	}
-	return (
-		<div className="App">
-			<div class="container">
-				<Logo />
-				<Router>
-					<Routes>
-						<Route path="/" element={<SignUpPage />} />
-						<Route
-							path="/signup/createprofile"
-							element={<ProfilePage handleUserInfo={handleUserInfo} />}
-						/>
-						<Route
-							path="/feed"
-							element={
-								<FeedPage username={username} pronouns={pronouns} bio={bio} />
-							}
-						/>
-					</Routes>
-				</Router>
-			</div>
-		</div>
-	);
+  function handleUserInfo(un, pp, bb) {
+    setUsername(un);
+    setPronouns(pp);
+    setBio(bb);
+  }
+  return (
+    <div className="App">
+      <div class="container">
+        <Logo />
+        <Router>
+          <Routes>
+            <Route path="/" element={<SignUpPage />} />
+
+            <Route
+              path="/signup/createprofile"
+              element={<ProfilePage handleUserInfo={handleUserInfo} />}
+            />
+            <Route
+              path="/feed"
+              element={
+                <FeedPage username={username} pronouns={pronouns} bio={bio} />
+              }
+            />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/login/userfeed" element={<LoginFeedPage />} />
+          </Routes>
+        </Router>
+      </div>
+    </div>
+  );
 }
 
 export default App;
