ef02e5045eb1c0a3b96cf010bdae7bd159b4c79d
kbalsom <100210446+kbalsom@users.noreply.github.com>
2022-08-11 16:45:55 -0230

    Add files via upload

---
+++ components/UserFeedPicture.jsx
@@ -0,0 +1,8 @@
+import "../stylesheets/userfeedpicture.css";
+import useravatar from "../images/useravatar.png";
+
+function UserFeedPicture() {
+  return <img width="80" height="80" src={useravatar} alt="Avatar" />;
+}
+
+export default UserFeedPicture;
