1cb509f1f265f60a271bcfb9ddc38383398deb6b
kbalsom <100210446+kbalsom@users.noreply.github.com>
2022-08-11 10:47:15 -0230

    Add files via upload

---
+++ components/UserPicture.jsx
@@ -0,0 +1,8 @@
+import "../stylesheets/UserPicture.css";
+import avatar from "../images/avatar.png";
+
+function UserPicture() {
+  return <img width="80" height="80" src={avatar} alt="Logo" />;
+}
+
+export default UserPicture;
