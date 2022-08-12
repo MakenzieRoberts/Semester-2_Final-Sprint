a0b77213aa86c99e14fc2e791709fdb5583862a9
MakenzieRoberts <100213075+MakenzieRoberts@users.noreply.github.com>
2022-08-12 13:23:28 -0230

    Removed inline width/height from UserFeedPicture

--- components/UserFeedPicture.jsx
+++ components/UserFeedPicture.jsx
@@ -2,7 +2,7 @@ import "../stylesheets/userfeedpicture.css";
 import useravatar from "../images/useravatar.png";
 
 function UserFeedPicture() {
-  return <img width="80" height="80" src={useravatar} alt="Avatar" />;
+  return <img src={useravatar} alt="Avatar" />;
 }
 
 export default UserFeedPicture;
