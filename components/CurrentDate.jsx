bb0dc177845b0b7e656a539907168a63e39ca45a
Makenzie Roberts <makenzie.roberts@keyin.com>
2022-08-12 23:06:04 -0230

    Add .padStart() to date for 2-digit minute format

--- components/CurrentDate.jsx
+++ components/CurrentDate.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 
 export default function CurrDate() {
-  const current = new Date();
-  const date = `${current.getDate()} ${current.getFullYear()}, ${current.getHours()}:${current.getMinutes()}`;
+	const current = new Date();
+	const date = `${current.getDate()} ${current.getFullYear()}, ${current.getHours()}:${String(
+		current.getMinutes()
+	).padStart(2, "0")}`;
 
-  const month = current.toLocaleString("default", { month: "short" });
+	const month = current.toLocaleString("default", { month: "short" });
 
-  return (
-    <div className="date">
-      <h1>
-        {month} {date}
-      </h1>
-    </div>
-  );
+	return (
+		<div className="date">
+			<h1>
+				{month} {date}
+			</h1>
+		</div>
+	);
 }
