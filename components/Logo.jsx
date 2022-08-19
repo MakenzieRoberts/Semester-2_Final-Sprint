2025be8996fa6a3d759cd37b55a2af8d824de886
kbalsom <100210446+kbalsom@users.noreply.github.com>
2022-08-19 15:36:09 -0230

    Update Logo.jsx

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -1,3 +1,7 @@
+// Project by: Kara Balsom, Makenzie Roberts, & David Turner
+//Date Written: August 8, 2022
+//File Name: Team1_Sprint2.zip
+
 import "../stylesheets/signuppage.css";
 import "../stylesheets/logo.css";
 import logo from "../images/logo.png";
